e1dd45a245a8e862d02523474c51860cf303939f
Luiz Falcao <luizotavio-falcao@hotmail.com>
2021-10-20 06:06:38 -0300

    Create CartItem component

---
+++ src/components/CartItem/CartItem.js
@@ -0,0 +1,27 @@
+import * as S from './styles';
+
+const CartItem = ({ image, name, price }) => {
+  return (
+    <S.CartItem>
+      <div>
+        <S.Image src={image} alt={name} />
+      </div>
+
+      <S.ProductInfo>
+        <p>{name}</p>
+      </S.ProductInfo>
+
+      <S.CountContainer>
+        <S.Decrement>-</S.Decrement>
+        <input type="number" defaultValue={0} />
+        <S.Increment>+</S.Increment>
+      </S.CountContainer>
+
+      <S.Price>{price}</S.Price>
+
+      <S.Remove>X</S.Remove>
+    </S.CartItem>
+  );
+};
+
+export default CartItem;
---
+++ src/components/CartItem/styles.jsx
@@ -0,0 +1,67 @@
+import styled from 'styled-components';
+
+export const CartItem = styled.div`
+  background: #000;
+  display: flex;
+  align-items: center;
+  padding: 1rem;
+  margin: 1rem 0;
+  border-radius: 0.25rem;
+
+  & input,
+  button {
+    width: 30px;
+    height: 30px;
+    text-align: center;
+  }
+
+  /* Remove input arrows 
+  ** Chrome, Safari, Edge, Opera */
+  & input::-webkit-outer-spin-button,
+  input::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
+  /* Firefox */
+  & input[type='number'] {
+    -moz-appearance: textfield;
+  }
+`;
+
+export const Image = styled.img`
+  width: 64px;
+  height: 64px;
+  object-fit: cover;
+`;
+
+export const ProductInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  margin: 0 1rem;
+  flex-grow: 1;
+  padding-bottom: 0.5rem;
+`;
+
+export const CountContainer = styled.div`
+  & button {
+    background: #51ba7d;
+  }
+`;
+
+export const Price = styled.span`
+  margin: 0 2rem;
+`;
+
+export const Decrement = styled.button`
+  border-radius: 0.25rem 0 0 0.25rem;
+`;
+
+export const Increment = styled.button`
+  border-radius: 0 0.25rem 0.25rem 0;
+`;
+
+export const Remove = styled.button`
+  background: #c33939;
+  margin-right: 1rem;
+`;
