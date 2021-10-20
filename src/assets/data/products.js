8e8338a0ebcbc8227474395df0e8e4017bda47d6
Luiz Falcao <luizotavio-falcao@hotmail.com>
2021-10-20 10:36:49 -0300

    Update product data

--- src/assets/data/products.js
+++ src/assets/data/products.js
@@ -2,16 +2,19 @@ import logo from '../images/carina.png';
 
 const products = [
   {
+    id: '0',
     image: logo,
     name: 'CarinaOS Student',
     price: '19.99',
   },
   {
+    id: '1',
     image: logo,
     name: 'CarinaOS Home',
     price: 49.99,
   },
   {
+    id: '2',
     image: logo,
     name: 'CarinaOS Pro',
     price: 99.99,
